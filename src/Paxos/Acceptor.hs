e266fd0e454161e976439cdcd130ef201f2af530
v6v <hzlez@163.com>
2019-01-07 16:14:37 +0800

    add a simple server

--- src/Paxos/Acceptor.hs
+++ src/Paxos/Acceptor.hs
@@ -1,2 +1,13 @@
 module Paxos.Acceptor where
 
+import           Paxos.Type
+
+
+data AcceptorState = AcceptorState {
+  ballotNum         :: BallotNum
+, acceptedProposals :: AcceptedProposals
+  }
+
+
+onMsg :: AcceptorState -> AcceptorState
+onMsg = undefined
